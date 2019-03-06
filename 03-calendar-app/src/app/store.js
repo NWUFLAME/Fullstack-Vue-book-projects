c84f55c2bd28817f14a37dbc25b3620736c0b4f6
AlexandreGuerra <agmguerra@gmail.com>
2019-03-06 20:13:55 -0300

    segunda aplicação

---
+++ 03-calendar-app/src/app/store.js
@@ -0,0 +1,19 @@
+import { seedData } from "./seed.js";
+
+export const store = {
+  state: {
+    seedData: seedData
+  },
+  getActiveDay: function() {
+      return this.state.seedData.find((day) => day.active);
+  },
+  setActiveDay (dayId) {
+      this.state.seedData.map((dayObj) => {
+          dayObj.id === dayId ? dayObj.active = true : dayObj.active = false;
+      });
+  },
+  submitEvent: function(eventDetails) {
+      const activeDay = this.getActiveDay();
+      activeDay.events.push({ "details": eventDetails, "edit": false});
+  }
+};
