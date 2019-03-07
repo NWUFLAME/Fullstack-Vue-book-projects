3a5cb0b0683dc5b453545e986941a9db64468f43
Alexandre Guerra <agmguerra@gmail.com>
2019-03-07 14:34:02 -0300

    app custon event

---
+++ 04-custom-events/app/main.js
@@ -0,0 +1,38 @@
+const inputComponent = {
+  template: `<input class="input is-small" type="text" 
+                v-model="input" 
+                :placeholder="placeholder"
+                 @keyup.enter="monitorEnterKey"/>`,
+  props: ['placeholder'],
+  data () {
+    return {
+        input: ''
+    }
+  },
+  methods: {
+    monitorEnterKey() {
+        this.$emit('add-note', {
+            note: this.input,
+            timestamp: new Date().toLocaleString()
+        });
+        this.input = '';
+    }
+  }
+};
+new Vue({
+  el: "#app",
+  data: {
+    notes: [],
+    timestamps: [],
+    placeholder: 'Enter a note'
+  },
+  methods: {
+    addNote(event) {
+        this.notes.push(event.note);
+        this.timestamps.push(event.timestamp);
+    }
+  },
+  components: {
+    "input-component": inputComponent
+  }
+});
