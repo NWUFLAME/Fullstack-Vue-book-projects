3629bf167a9ddb235e8f5ab33380616791e556f1
Alexandre Guerra <agmguerra@gmail.com>
2019-03-20 17:18:54 -0300

    ajustes

--- 14-vuex-app/main.js
+++ 14-vuex-app/main.js
@@ -1,38 +1,37 @@
-
 const InputForm = {
   template: `
   <div class="input-form">
     <form @submit="submitForm" class="ui form">
       <div class="field">
         <label>New Item</label>
-        <input :value="newItem" type="text" placeholder="Add an item!" />
+        <input :value="newItem" @input="onInputChange" name="NEW_ITEM" type="text" placeholder="Add an item!" />
         <span style="float: right">{{ newItemLength }}/20</span>
-        <span style="color: red">{{ newItem }}</span>
+        <span style="color: red">{{ fieldErrors.newItem }}</span>
         <span v-if="isNewItemInputLimitExceeded" style="color: red; display: block">
           Must be under twenty characters
         </span>
       </div>
       <div class="field">
         <label>Email</label>
-        <input :value="email" type="text" placeholder="What's your email?" />
+        <input :value="email" @input="onInputChange" name="EMAIL" type="text" placeholder="What's your email?" />
         <span style="color: red">{{ fieldErrors.email }}</span>
       </div>
       <div class="field">
         <label>Urgency</label>
-        <select :value="urgency" class="ui fluid search dropdown">
+        <select :value="urgency"  @change="onInputChange" name="URGENCY" class="ui fluid search dropdown">
           <option disabled value="">Please select one</option>
           <option>Nonessential</option>
           <option>Moderate</option>
           <option>Urgent</option>
         </select> 
-        <span style="color: red">{{ urgency }}</span>
+        <span style="color: red">{{ fieldErrors.urgency }}</span>
         <span v-if="isNotUrgent" style="color: red; display: block">
           Must be moderate to urgent
         </span>
       </div>
       <div class="field">
         <div class="ui checkbox">
-          <input :checked="termsAndConditions" type="checkbox" />
+          <input :checked="termsAndConditions" @change="onInputChange" name="TERMS_AND_CONDITIONS" type="checkbox" />
           <label>I accept the terms and conditions</label>
           <span style="color: red">{{ fieldErrors.termsAndConditions }}</span>
         </div>
@@ -63,33 +62,36 @@ const InputForm = {
     };
   },
   computed: Vuex.mapGetters({
-    newItem: 'newItem',
-    newItemLength: 'newItemLength',
-    isNewItemInputLimitExceeded: 'isNewItemInputLimitExceeded',
-    email: 'email',
-    urgency: 'urgency',
-    isNotUrgent: 'isNotUrgent',
-    termsAndConditions: 'termsAndConditions',
-    items: 'items'
+    newItem: "newItem",
+    newItemLength: "newItemLength",
+    isNewItemInputLimitExceeded: "isNewItemInputLimitExceeded",
+    email: "email",
+    urgency: "urgency",
+    isNotUrgent: "isNotUrgent",
+    termsAndConditions: "termsAndConditions",
+    items: "items"
   }),
   methods: {
+    onInputChange(evt) {
+      const element = evt.target;
+      const value =
+        element.name === "TERMS_AND_CONDITIONS"
+          ? element.checked
+          : element.value;
+      this.$store.commit(`UPDATE_${element.name}`, value);
+    },
     submitForm(evt) {
       evt.preventDefault();
-
-      this.fieldErrors = this.validateForm(this.fields);
+      this.fieldErrors = this.validateForm(this.$store.state.fields);
       if (Object.keys(this.fieldErrors).length) return;
-
-      const items = [...this.items, this.fields.newItem];
-
+      const items = [
+        ...this.$store.state.items,
+        this.$store.state.fields.newItem
+      ];
       this.saveStatus = "SAVING";
-      apiClient
-        .saveItems(items)
+      this.$store
+        .dispatch("saveItems", items)
         .then(() => {
-          this.items = items;
-          this.fields.newItem = "";
-          this.fields.email = "";
-          this.fields.urgency = "";
-          this.fields.termsAndConditions = false;
           this.saveStatus = "SUCCESS";
         })
         .catch(err => {
@@ -117,10 +119,14 @@ const InputForm = {
     }
   },
   created() {
-    (this.loading = true),
-      apiClient.loadItems().then(items => {
-        this.items = items;
+    this.loading = true;
+    this.$store
+      .dispatch("loadItems")
+      .then(response => {
         this.loading = false;
+      })
+      .catch(error => {
+        console.log(error);
       });
   }
 };
