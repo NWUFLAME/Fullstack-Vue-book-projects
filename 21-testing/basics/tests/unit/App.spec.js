9dc6d52bb30d196f8d7808e2dca47234c7be277a
Alexandre Guerra <agmguerra@gmail.com>
2019-03-29 17:53:10 -0300

    teste

--- 21-testing/basics/tests/unit/App.spec.js
+++ 21-testing/basics/tests/unit/App.spec.js
@@ -1,7 +1,21 @@
+import Vue from 'vue';
+import App from '@/App';
 import { expect } from 'chai';
 
 describe('App.vue', () => {
   it('should run the correct contents', () => {
-     
+    const Constructor = Vue.extend(App);
+    const vm = new Constructor().$mount();
+
+    expect(
+      vm.$el.querySelector('.ui.selectable thead tr th').textContent
+    ).to.contain('Items');
+
+    expect(
+      vm.$el.querySelector('.ui.button').textContent
+    ).to.contain('Add');
+    expect(
+      vm.$el.querySelector('.ui.label').textContent
+    ).to.contain('Remove all');
   });
 });
